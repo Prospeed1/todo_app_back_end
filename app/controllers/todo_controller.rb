439683df37c7a86122f5aa3338bdf74e6dd78790
Prospeed <daeshaunmorrison@gmail.com>
2018-05-12 19:27:09 +0000

    Avoided multiple page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,4 +2,31 @@ class TodoController < ApplicationController
     def index
     
     end
+    def show
+        todo_id = params[:id]
+            if todo_id == "1"
+                @description = "Do H/W"
+                @pomodoro_estimate = 1
+            elsif todo_id == "2"
+                @description = "Run track :)"
+                @pomodoro_estimate = 5
+            elsif todo_id == "3"
+                @description = "Play chess"
+                @pomodoro_estimate = 4
+            elsif todo_id == "4"
+                @description = "Read book"
+                @pomodoro_estimate = 3
+            elsif todo_id == "5"
+                @description = "Use Notepad"
+                @pomodoro_estimate = 1
+            elsif todo_id == "6"
+                @description = "Watch anime"
+                @pomodoro_estimate = 4
+            else
+                @description = "Find a Hobby :P"
+                @pomodoro_estimate = 999
+                                                
+                
+            end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'todo/index' => 'todo#index'
+  get 'todo/show/:id' => 'todo#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
