f187efccb757db8835b6f72a7ba7c42577dc05b4
Prospeed <daeshaunmorrison@gmail.com>
2018-05-13 15:48:55 +0000

    Made project less hard-coded

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,30 +3,16 @@ class TodoController < ApplicationController
     
     end
     def show
+        # Gets number user typed in URL
         todo_id = params[:id]
-            if todo_id == "1"
-                @description = "Do H/W"
-                @pomodoro_estimate = 1
-            elsif todo_id == "2"
-                @description = "Run track :)"
-                @pomodoro_estimate = 5
-            elsif todo_id == "3"
-                @description = "Play chess"
-                @pomodoro_estimate = 4
-            elsif todo_id == "4"
-                @description = "Read book"
-                @pomodoro_estimate = 3
-            elsif todo_id == "5"
-                @description = "Use Notepad"
-                @pomodoro_estimate = 1
-            elsif todo_id == "6"
-                @description = "Watch anime"
-                @pomodoro_estimate = 4
-            else
-                @description = "Find a Hobby :P"
-                @pomodoro_estimate = 999
-                                                
-                
-            end
+        
+        #Grab the todo with that id from the database
+        todo = Todo.find_by_id(todo_id)
+        
+        #Set the variable shared with template to the values we got
+        #from database
+        @todo_description = todo.description
+        @todo_pomodoro_estimate = todo.pomodoro_estimate
+            
     end
 end
